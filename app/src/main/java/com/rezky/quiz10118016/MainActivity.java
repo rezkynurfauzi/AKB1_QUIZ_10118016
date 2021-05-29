a162dbfc845961abc4e6d7581c33f4a62ccae613
Rezky Nur Fauzi <rezkynurfauzi80@gmail.com>
2021-05-29 12:32:10 +0700

    AKB1 QUIZ NIM : Rezky Nur Fauzi Nama : Rezky Nur Fauzi Kelas : IF-01

---
+++ app/src/main/java/com/rezky/quiz10118016/MainActivity.java
@@ -0,0 +1,21 @@
+package com.rezky.quiz10118016;
+
+/*
+ * NIM   : 10118016
+ * NAMA  : Rezky Nur Fauzi
+ * KELAS : IF1
+ * TGL   : 29/05/2021
+ * */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
